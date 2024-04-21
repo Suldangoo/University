679bb53d44f398e16eb47b757defb04251c01f24
suldangoo <suldangoo@gmail.com>
2024-04-21 19:59:33 +0900

    add commit

--- 웹개발실무/week07/03async/02async/blocking-02-non.js
+++ 웹개발실무/week07/03async/02async/blocking-02-non.js
@@ -1,6 +1,7 @@
 // ./03async/02async/blocking-02-non.js
 const fs = require("fs");
 
+// 비동기로 처리
 const data = fs.readFile("example.txt", "utf8", (err, data) => {
   if (err) {
     console.error(err);
