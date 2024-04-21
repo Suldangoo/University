a9a0e68b609f2999153478730f7fe16e6c20f79f
suldangoo <suldangoo@gmail.com>
2024-04-21 20:13:00 +0900

    add study

--- 웹개발실무/week02/allow-function.js
+++ 웹개발실무/week02/allow-function.js
@@ -1,23 +1,27 @@
-// // 1)
-// function add(a, b) {
-//     return  a + b ;
-// }
+// 1)
+function add1(a, b) {
+    return  a + b ;
+}
 
-// // 2)
-// const add = function (a, b) {
-//     return  a + b ;
-// }
+// 2)
+const add2 = function (a, b) {
+    return  a + b ;
+}
 
-// // 3)
-// const add = (a, b)  => {
-//     return  a + b ;
-// }
+// 3)
+const add3 = (a, b)  => {
+    return  a + b ;
+}
 
-// // 4)
-// const add = (a, b) => {   a + b ; }
+// 4)
+const add4 = (a, b) => { return a + b; }
 
 // 5)
-const add = (a, b) => a + b; 
+const add5 = (a, b) => a + b; 
 
 // 함수호출
-console.log(add(2, 4));
+console.log(add1(2, 4));
+console.log(add2(2, 4));
+console.log(add3(2, 4));
+console.log(add4(2, 4));
+console.log(add5(2, 4));
--- 웹개발실무/week07/03async/02async/blocking-01.js
+++ 웹개발실무/week07/03async/02async/blocking-01.js
@@ -1,6 +1,6 @@
 // ./03async/02async/async-01.js
 const fs = require("fs");
 
-const data = fs.readFileSync("example.txt", "utf8"); // 블로킹
+const data = fs.readFileSync("example.txt", "utf8"); // 블로킹, 동기
 console.log(data);          // 파일 읽기가 끝날 때까지 대기
 console.log("코드 끝!");     // 파일을 읽고 내용을 표시할 때까지 대기
