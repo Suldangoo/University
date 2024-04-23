364d3cd9953440ba56aca40d2d57b65663013b76
JunHyeong Park <suldangoo@gmail.com>
2024-04-24 00:25:15 +0900

    add study

--- 웹개발실무/week05/04express-test/app-01.js
+++ 웹개발실무/week05/04express-test/app-01.js
@@ -3,7 +3,20 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send("HOME");
+    res.send("Hello World");
+});
+
+// 해당 라우트의 파람스 입력하면 반영
+app.get('/sayhello/:name', (req, res) => {
+    res.status(200);
+    let name = req.params.name;
+
+    res.send(`<h1>Hello, ${name}!`);
+});
+
+// 위 라우트가 아닌 모든 라우트에서 에러 표시
+app.use((req, res) => {
+    res.status(404).send('File Not Found');
 });
 
 app.listen(port, () => {
