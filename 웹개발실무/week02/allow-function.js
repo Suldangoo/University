5d08c095948bf0bdb8ea2e87fbd6c1a76d5d40c7
suldangoo <suldangoo@gmail.com>
2024-04-23 14:55:41 +0900

    add func

--- 웹개발실무/week02/allow-function.js
+++ 웹개발실무/week02/allow-function.js
@@ -16,12 +16,18 @@ const add3 = (a, b)  => {
 // 4)
 const add4 = (a, b) => { return a + b; }
 
+// const add5 = (a, b) => return a + b; 리턴은 반드시 중괄호 필요
+
+const add6 = (a, b) => { a + b; } // 효과 없음
+
 // 5)
-const add5 = (a, b) => a + b; 
+const add7 = (a, b) => a + b; 
 
 // 함수호출
 console.log(add1(2, 4));
 console.log(add2(2, 4));
 console.log(add3(2, 4));
 console.log(add4(2, 4));
-console.log(add5(2, 4));
+//console.log(add5(2, 4));
+console.log(add6(2, 4));
+console.log(add7(2, 4));
