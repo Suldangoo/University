2ad28d7e6fdc2bdc3353f339d080ad7e211817c2
suldangoo <suldangoo@gmail.com>
2024-06-12 20:04:13 +0900

    add files

--- 웹개발실무/week13/02student/student-crud-stu-01.js
+++ 웹개발실무/week13/02student/student-crud-stu-01.js
@@ -1,6 +1,6 @@
 // ./02student/student-crud.js
 const MongoClient = require("mongodb").MongoClient;
-// const uri = '    ?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://admin:1234@cluster0.syxnfif.mongodb.net/testDB?retryWrites=true&w=majority';
 
 const client = new MongoClient(uri);
 
@@ -10,15 +10,29 @@ async function run() {
         console.log('DB Connected');
 
         // 컬렉션
+        const collection = client.db('testDB').collection('student');
 
         // 추가
+        await collection.insertOne( { name : 'jhPark', age : 22 } );
+        console.log('입력 완료');
 
         // 검색
+        const documents = await collection.find({name : "jhPark"}).toArray();
+        console.log('문서 검색 : ', documents);
 
         // 수정
+        await collection.updateOne({name : 'jhPark'}, {$set : {age : 33}});
+        console.log('수정 완료');
+
+        const updatedDocuments = await collection.find(
+          { name : 'jhPark' }
+        ).toArray();
+        console.log('수정된 문서 : ', updatedDocuments);
 
         // 삭제
-        
+        await collection.deleteOne( {name : 'jhPark'} );
+        console.log('삭제 완료');
+
         // 연결 종료
         await client.close();
     } catch (err) {
--- 웹개발실무/week13/03user/user-crud-stu-01.js
+++ 웹개발실무/week13/03user/user-crud-stu-01.js
@@ -1,9 +1,8 @@
 // ./03person/user-crud.js
 const express = require("express");
 const bodyParser = require("body-parser");
-
-
-
+const mongoose = require("mongoose");
+const User = require("./models/userModel");
 
 mongoose.set("strictQuery", false);
 
@@ -12,11 +11,9 @@ app.use(bodyParser.json());
 
 app.listen(3000, async () => {
     console.log("3000번 포트에서 서버 실행중");
-//    const mongodbUri = '             /testDB?retryWrites=true&w=majority';
+    const mongodbUri = 'mongodb+srv://admin:1234@cluster0.syxnfif.mongodb.net/testDB?retryWrites=true&w=majority';
 
-    // mongoose.connect(mongodbUri, { useNewUrlParser: true })
-
-    
+    mongoose.connect(mongodbUri).then(console.log('DB Connected'));
 });
 
 // user 전체 정보 검색
@@ -27,22 +24,34 @@ app.get('/user', async (req, res) => {
 });
 
 // 특정 name 으로 검색
-app.get('', async (req, res) => {
-
+app.get('/user/:name', async (req, res) => {
+    const user = await User.findOne(
+        { name : req.params.name });
+    console.log(user);
+    res.send(user);
 });
 
 // 추가
-app.post('', async (req, res) => {
-
+app.post('/user', async (req, res) => {
+    const user = new User(req.body);
+    await user.save();
+    res.send(user);
 });
 
 // 수정
-app.put('', async (req, res) => {
-
+app.put('/user/:name', async (req, res) => {
+    const user = await User.findOneAndUpdate(
+        { name : req.params.name }, 
+        { $set: req.body }, 
+        { new: true }
+    );
+    console.log(user);
+    res.send(user);
 });
+
 
 // 삭제
-app.delete('', async (req, res) => {
-
+app.delete('/user/:name', async (req, res) => {
+    await User.deleteMany({ name : req.params.name });
+    res.send({success: true})
 });
-
