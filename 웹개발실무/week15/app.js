596c3d9ac2145be40100263ebd8c470b24bc20f8
suldangoo <suldangoo@gmail.com>
2024-06-19 19:31:17 +0900

    add file

--- 웹개발실무/week15/app.js
+++ 웹개발실무/week15/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dbConnect = require("./config/dbConnect");
-
+const methodOverride = require("method-override");
 const port = 3000;
 
 const app = express();
@@ -9,6 +9,8 @@ app.set("view engine", "ejs");
 app.set("views", "./views");
 
 app.use(express.static("./public"));
+
+app.use(methodOverride("_method"));
 
 dbConnect();
 
